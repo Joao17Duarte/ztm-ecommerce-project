d2c612571dd9e928381ea8aecb3b7726c51aaa47
Joao Duarte <joaopd17@hotmail.com>
2023-12-02 13:25:38 +0100

    Added Product context and updated the App and Index files with the logic needed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router'
 import Home from './routes/Home/Home.component'
 import Navigation from './routes/Navigation/Navigation.component'
 import Authentication from './routes/Authentication/Authentication.component'
+import Shop from './routes/Shop/Shop.component'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
